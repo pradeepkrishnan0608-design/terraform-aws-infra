5c1fb8a89169e340d33d0c22daed4519fe500398
Pradeep Kumar <pradeepkrishnan0608@gmail.com>
2026-06-24 03:57:46 +0530

    Fixed Terraform configuration and added gitignore

--- main.tf
+++ main.tf
@@ -1,10 +1,14 @@
- required_providers {
+ 
+
+terraform{
+
+required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 6.0"
     }
   }
-
+}
 
 provider "aws" {
   region = "ap-south-1"
